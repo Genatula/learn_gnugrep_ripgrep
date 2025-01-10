6e55e6a7a3a8a584a5143f0cc4c5463d584b2d86
Ilnas <genatula@protonmail.com>
2025-01-10 22:04:30 +0300

    Solved first chapter exercises

--- exercises/frequently-used-options/solution.sh
+++ exercises/frequently-used-options/solution.sh
@@ -1,7 +1,15 @@
+#!/usr/bin/bash
+
 grep an sample.txt
 grep -w do sample.txt
 grep -F fruit[0] code.txt
 grep -m 2 t sample.txt
 grep -v -m 3 he sample.txt
 grep -n do sample.txt
-1
+grep -cx '' sample.txt
+grep -F -f terms.txt sample.txt code.txt
+grep -Hn amigo sample.txt
+grep -l apple sample.txt code.txt
+grep -HFnx -f lines.txt sample.txt code.txt
+grep -Fcv -f terms.txt code.txt sample.txt
+exit 0
